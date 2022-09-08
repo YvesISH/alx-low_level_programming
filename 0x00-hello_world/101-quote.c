5fe016084c3a61e9cc5b2b6f965d989b64ec60df
YvesISH <ivanmutware999@gmail.com>
2022-09-08 08:08:21 -0700

    lastly

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+        write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
