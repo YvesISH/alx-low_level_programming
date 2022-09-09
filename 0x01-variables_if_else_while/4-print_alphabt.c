5b077dc0eb32c82323728a5436ac16b0db9adff1
YvesISH <ivanmutware999@gmail.com>
2022-09-09 09:17:28 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,13 @@
 int main(void)
 {
 	char low, e, q;
+
 	e = 'e';
 	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low !=e && low !=q)
+		if (low != e && low != q)
 			putchar(low);
 	}
 	putchar('\n');
