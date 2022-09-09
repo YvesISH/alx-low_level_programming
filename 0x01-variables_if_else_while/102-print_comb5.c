8f8c2d32048f1fc401ca0ea32e60998746ed674f
YvesISH <ivanmutware999@gmail.com>
2022-09-09 10:08:09 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,7 +25,7 @@ int main(void)
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 == 98 &&num2 == 99)
+			if (num1 == 98 && num2 == 99)
 				continue;
 
 			putchar(',');
