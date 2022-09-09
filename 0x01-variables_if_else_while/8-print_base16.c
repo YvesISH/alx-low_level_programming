f29b60999a9c320765ab79bf92daabb684ca6f50
YvesISH <ivanmutware999@gmail.com>
2022-09-09 09:06:33 -0700

    9updated

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,9 +13,9 @@ int main(void)
 	int d;
 	char low;
 
-	for (d = '0'; d<= '9'; d++)
+	for (d = '0'; d <= '9'; d++)
 		putchar(d);
-	for (low ='a'; low <= 'f'; low++)
+	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
 	putchar('\n');
 
