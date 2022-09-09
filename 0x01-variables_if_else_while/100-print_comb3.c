9c8310867a0093a0bcbddeffcbd39067f210f9f0
YvesISH <ivanmutware999@gmail.com>
2022-09-09 10:04:11 -0700

    Bonus

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+	int digit1, dig;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for dig = digit1 + 1; dig < 10; dig++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((dig % 10) + 0);
+
+			if (digit1 == 8 && dig == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return(0);
+}
