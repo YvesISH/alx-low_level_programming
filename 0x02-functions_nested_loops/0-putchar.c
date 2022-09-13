197cc27df956a1da1e2ebd4c51dd1aed357dcd5c
YvesISH <ivanmutware999@gmail.com>
2022-09-12 22:59:56 -0700

    proj1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	char *xx = "_putchar";
+
+	while (*xx)
+	{
+		_putchar(*xx);
+		xx++;
+	}
+	_putchar('\n);
+
+	return (0);
+}
