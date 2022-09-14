f34cd25ff7eeb1bba15d8181b05e53eca5797c3a
YvesISH <ivanmutware999@gmail.com>
2022-09-14 08:40:42 -0700

    101 proj

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/** main -m  lists all the naturalnumbers below 1024
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i %3) == 0 || (i % 5) ==0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
