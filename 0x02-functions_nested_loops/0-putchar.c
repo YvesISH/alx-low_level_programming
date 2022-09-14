17643f9befda44ee5ba85c067ea7167092677cd6
YvesISH <ivanmutware999@gmail.com>
2022-09-14 05:55:22 -0700

    Proj 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
  *     */
 int main(void)
 {
-	char *xx = "_putchar";
-
-	while (*xx)
-	{
-		_putchar(*xx);
-		xx++;
-	}
-	_putchar('\n);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
