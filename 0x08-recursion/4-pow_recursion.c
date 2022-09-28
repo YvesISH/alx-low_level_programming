0cf81e1fe2f28981054d3ae74cf81827e28630f6
YvesISH <ivanmutware999@gmail.com>
2022-09-28 04:13:18 -0700

    Mandatory Task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _pow_recursion - Raise x to power of y
- * x: number
- * y: number
+ * @x: number
+ * @y: number
  * Return: Always a raised number
  */
 int _pow_recursion(int x, int y)
