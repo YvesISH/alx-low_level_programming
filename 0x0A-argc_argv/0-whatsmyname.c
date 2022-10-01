897f1153e04c92d3e4657a6addc68f23c461816a
YvesISH <ivanmutware999@gmail.com>
2022-09-30 22:29:14 -0700

    Mandatory Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
- * main - print the name of the program
- * @argc: count arguments
- * @argv: arguments
- *
- * Return: Always 0
- *
+ *  * main - program that prints its name,
+ *   * followed by a new line.
+ *    * @argc: number of command line arguments.
+ *     * @argv: array that contains the program command line arguments.
+ *      * Return: Always 0 .
  */
-int main(int argc, char *argv[])
-{
-	(void) argc;
-	printf("%\n", argv[0]);
 
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
 	return (0);
 }
